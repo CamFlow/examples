c1f54326c595cf3ee92a6cb4b7280e0d2a601488
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2016-10-30 13:15:36 -0700

    simplify example

--- provenance/file.c
+++ provenance/file.c
@@ -18,26 +18,22 @@ int cp(const char *to, const char *from)
     ssize_t nread;
     int saved_errno;
 
-    disclose_activity_ProvJSON("\"rdt:name\":\"fd_from = open(from, O_RDONLY);\", \"rdt:scriptLine\":\"22\"", &current);
     fd_from = open(from, O_RDONLY);
 
 
     if (fd_from < 0)
         return -1;
 
-    disclose_activity_ProvJSON("\"rdt:name\":\"fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);\", \"rdt:scriptLine\":\"29\"", &current);
     fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
     if (fd_to < 0)
         goto out_error;
 
     while (nread = read(fd_from, buf, sizeof buf), nread > 0)
     {
-        disclose_activity_ProvJSON("\"rdt:name\":\"while (nread = read(fd_from, buf, sizeof buf), nread > 0)\", \"rdt:scriptLine\":\"36\"", &current);
         char *out_ptr = buf;
         ssize_t nwritten;
 
         do {
-            disclose_activity_ProvJSON("\"rdt:name\":\"nwritten = write(fd_to, out_ptr, nread);\", \"rdt:scriptLine\":\"40\"", &current);
             nwritten = write(fd_to, out_ptr, nread);
 
             if (nwritten >= 0)
