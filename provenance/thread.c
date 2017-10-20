4d5074b7d10be1fff467136c80a8c3bd86d114c7
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-10-20 14:34:46 +0000

    update example

--- provenance/thread.c
+++ provenance/thread.c
@@ -5,6 +5,9 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+#include "provenance.h"
 
 void *print_message_function( void *ptr );
 
@@ -14,6 +17,15 @@ int main()
      char *message1 = "Thread 1";
      char *message2 = "Thread 2";
      int  iret1, iret2;
+
+     if(provenance_set_tracked(true)){
+       printf("Failed Tracking, error %d\n", errno);
+       printf("%s\n\n",strerror(errno));
+     }
+     if(provenance_set_propagate(true)){
+       printf("Failed propagate, error %d\n", errno);
+       printf("%s\n\n",strerror(errno));
+     }
     /* Create independent threads each of which will execute function */
      iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
      iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
@@ -26,12 +38,11 @@ void *print_message_function( void *ptr )
 {
      char *message;
      message = (char *) ptr;
-     printf("%s \n", message);
      char file_name[100];
      int cc = snprintf(file_name,100,"/tmp/output_threadid_%lu",syscall(SYS_gettid));
      file_name[cc] = '\0';
      FILE *fp;
      fp = fopen(file_name,"w");
-     fputs("test",fp);
+     fputs(message, fp);
      fclose(fp);
 }
