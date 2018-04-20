2cc55ff3f519b2074d2d00134a77399a422c4b55
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2018-04-20 05:24:14 +0100

    add only open example

---
+++ provenance/open.c
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "provenance.h"
+
+int main(){
+  int fd;
+
+  if(provenance_set_tracked(true)){
+    printf("Failed Tracking, error %d\n", errno);
+    printf("%s\n\n",strerror(errno));
+  }
+  if(provenance_set_propagate(true)){
+    printf("Failed propagate, error %d\n", errno);
+    printf("%s\n\n",strerror(errno));
+  }
+
+
+  fd = open("./LICENSE", O_RDONLY);
+}
